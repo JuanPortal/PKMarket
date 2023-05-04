d328c7160968b010e5a29ec22ebdf44325eb3899
Portal <juanportalg@gmail.com>
2023-05-04 15:46:02 -0700

    component renamed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { PKMarket } from './PKMarket'
+import { Banner } from './Banner'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <PKMarket />
+        <Banner />
     </React.StrictMode>
 )
