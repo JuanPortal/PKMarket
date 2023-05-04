a74c9a4afa20df0d89b15e6fad4a3e203f679f63
Portal <juanportalg@gmail.com>
2023-05-04 16:02:56 -0700

    Cover and Story created

---
+++ src/Cover.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Banner } from './Banner'
+import { Story } from './Story'
+
+export const Cover = () => {
+  return (
+    <section className='cover'>
+        <Banner />
+        <Story />
+    </section>
+  )
+}
