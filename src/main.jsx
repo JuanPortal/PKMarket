3d4c54e165890e1384dfddef7fa4cec4ac5a34c2
Portal <juanportalg@gmail.com>
2023-05-04 16:02:25 -0700

    Cover added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Banner } from './Banner'
+import { Cover } from './Cover'
+import './styles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Banner />
+        <Cover />
     </React.StrictMode>
 )
