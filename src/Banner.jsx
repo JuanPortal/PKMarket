94c4386e008861ccda8e5fb5bfccabe7d73f29b7
Portal <juanportalg@gmail.com>
2023-05-05 17:16:58 -0700

    socials added

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEbay, faFacebook, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export const Banner = () => {
   return (
-    <>
-        <div className='banner'></div>
-    </>
+    <div className='banner'>
+        <div className='icons'>
+          <a href="https://www.ebay.com/usr/thepkmarket" target='_blank'><FontAwesomeIcon icon={faEbay}/></a>
+          <a href="https://www.instagram.com/thepkmarket/" target='_blank'><FontAwesomeIcon icon={faInstagram} /></a>
+          <a href="https://www.facebook.com/PKMarket6IV" target='_blank'><FontAwesomeIcon icon={faFacebook} /></a>
+          <a href="https://github.com/JuanPortal/PKMarket" target='_blank'><FontAwesomeIcon icon={faGithub} /></a>
+        </div>
+    </div>
   )
 }
