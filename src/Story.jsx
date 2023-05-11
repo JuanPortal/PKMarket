dccd0be2cfc4f4d6a67b9c914b59f406e992c896
Portal <juanportalg@gmail.com>
2023-05-11 15:40:41 -0700

    updated

--- src/Story.jsx
+++ src/Story.jsx
@@ -4,9 +4,7 @@ export const Story = () => {
   return (
     <div className='story'>
         <div className='story-container'>
-            <div className='story-image'>
-                <img src="../images/people_trading.png" alt="People trading pokemon in a GameBoy Color" />
-            </div>
+            <img src="../images/people_trading.png" alt="People trading pokemon in a GameBoy Color" />
             <div className='story-text'>
                 <p>Never completed your Pokédex? Neither did I!</p>
                 <p>
@@ -14,7 +12,7 @@ export const Story = () => {
                     I didn't know what the process was or that the Link Cable even existed. 
                     When I finally learned how it worked as an adult, sadly my friends had already stopped playing. 
                     That's why now I help people complete their Pokédex in their own save files, 
-                    as well as providing strong competitive Pokémon for their battles and shiny pokémon for all the shiny hunters out there!
+                    aswell as providing strong competitive Pokémon for their battles and shiny pokémon for all the shiny hunters out there!
                 </p>
             </div>
         </div>
