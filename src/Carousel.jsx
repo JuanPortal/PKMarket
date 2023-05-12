ad49ba4462f097558f94a5bf2e4fc2f66c3ae1de
Portal <juanportalg@gmail.com>
2023-05-11 19:57:11 -0700

    alt added

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const Carousel = ({album}) => {
   return (
     <div className='carousel'>
-        <img src={`../images/screenshots/${album}/1.png`} alt="1" />
-        <img src={`../images/screenshots/${album}/2.png`} alt="2" />
-        <img src={`../images/screenshots/${album}/3.png`} alt="3" />
-        <img src={`../images/screenshots/${album}/4.png`} alt="4" />
+        <img src={`../images/screenshots/${album}/1.png`} alt="1st image" />
+        <img src={`../images/screenshots/${album}/2.png`} alt="2nd image" />
+        <img src={`../images/screenshots/${album}/3.png`} alt="3rd image" />
+        <img src={`../images/screenshots/${album}/4.png`} alt="4th image" />
     </div>
   )
 }
