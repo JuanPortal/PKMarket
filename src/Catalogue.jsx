c49719ef396fdbdd02cd0148c10d3de8af19aed2
Portal <juanportalg@gmail.com>
2023-05-11 19:57:35 -0700

    Availability added

--- src/Catalogue.jsx
+++ src/Catalogue.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Section } from './Section'
-
+import { Games } from './Games'
 
 export const Catalogue = () => {
   return (
@@ -10,6 +10,20 @@ export const Catalogue = () => {
         <Section type='Event' description='We have every event distribution pokémon from all gens'/>
         <Section type='Pokédex' description='Complete Living Dex, shiny or regular (Sent via HOME)'/>
         <Section type='Extra' description='Shiny eggs || Foreign Ditto for Masuda Method || Send pokemon from a PC/Phone emulator to a 3DS/Switch'/>
+        <div className='availability'>
+            <p>Available for all these games:</p>
+            <div className="games-container">
+            <Games name='XY' title='Pokémon X & Pokémon Y'/>
+            <Games name='ORAS' title='Pokémon Omega Ruby & Pokémon Alpha Sapphire'/>
+            <Games name='SM' title='Pokémon Sun & Pokémon Moon'/>
+            <Games name='USUM' title='Pokémon Ultra Sun & Pokémon Ultra Moon'/>
+            <Games name='LGPE' title="Pokémon Let's Go, Pikachu! & Pokémon Let's Go, Eevee!"/>
+            <Games name='SWSH' title='Pokémon Sword & Pokémon Shield'/>
+            <Games name='BDSP' title='Pokémon Brilliant Diamond & Pokémon Shining Pearl'/>
+            <Games name='PLA' title='Pokémon Legends Arceus'/>
+            <Games name='SV' title='Pokémon Scarlet & Pokémon Violet'/>
+            </div>
+        </div>
     </section>
   )
 }
