97de2d605047db90eae2cc002a7677b565e3742d
Portal <juanportalg@gmail.com>
2023-05-11 18:17:07 -0700

    Catalogue Section created

---
+++ src/Section.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Carousel } from './Carousel'
+
+
+export const Section = ({type, description}) => {
+  return (
+    <div className='catalogue-section'>
+        <div className='catalogue-titles'>
+            <h3>{type}</h3>
+            <h4>{description}</h4>
+        </div>
+        <Carousel album={type}/>
+    </div>
+  )
+}
