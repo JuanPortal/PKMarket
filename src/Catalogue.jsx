651e6c713ed86322321f49ff4981bdc4c169c723
Portal <juanportalg@gmail.com>
2023-05-11 19:18:30 -0700

    description updated

--- src/Catalogue.jsx
+++ src/Catalogue.jsx
@@ -6,10 +6,10 @@ export const Catalogue = () => {
   return (
     <section className='catalogue'>
         <h2>Catalogue</h2>
-        <Section type='Teams' description='Customizable team of 6 pokémon'/>
-        <Section type='Event' description='Every event pokemon available'/>
-        <Section type='Pokédex' description='Complete Living Dex, shiny or regular'/>
-        <Section type='Move' description='Send pokemon from a PC or Phone emulator to a 3DS or a Switch'/>
+        <Section type='Team' description='Customizable team of 6 pokémon, you can choose any pokémon in the game'/>
+        <Section type='Event' description='We have every event distribution pokémon from all gens'/>
+        <Section type='Pokédex' description='Complete Living Dex, shiny or regular (Sent via HOME)'/>
+        <Section type='Extra' description='Shiny eggs || Foreign Ditto for Masuda Method || Send pokemon from a PC/Phone emulator to a 3DS/Switch'/>
     </section>
   )
 }
