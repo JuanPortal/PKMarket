36ebc7d20a8f063c7f96e7047a115de9506328ef
Portal <juanportalg@gmail.com>
2023-05-11 18:17:52 -0700

    Catalogue improved

--- src/Catalogue.jsx
+++ src/Catalogue.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Listing } from './Listing'
+import { Section } from './Section'
+
 
 export const Catalogue = () => {
   return (
     <section className='catalogue'>
-        <Listing />
+        <h2>Catalogue</h2>
+        <Section type='Teams' description='Customizable team of 6 pokémon'/>
+        <Section type='Event' description='Every event pokemon available'/>
+        <Section type='Pokédex' description='Complete Living Dex, shiny or regular'/>
+        <Section type='Move' description='Send pokemon from a PC or Phone emulator to a 3DS or a Switch'/>
     </section>
   )
 }
