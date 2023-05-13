c80db83314d09a285d893efc3295d2f8d030419c
Portal <juanportalg@gmail.com>
2023-05-12 22:29:01 -0700

    Delivery component created

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { Cover } from './Cover'
 import { Catalogue } from './Catalogue'
-
-
+import { Delivery } from './Delivery'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Cover />
         <Catalogue />
+        <Delivery />
     </React.StrictMode>
 )
