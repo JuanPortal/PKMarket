3af74ce86a2ae38a9d3f9796aaf80004575e8996
Portal <juanportalg@gmail.com>
2023-05-19 00:20:16 -0700

    before using require()

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -7,6 +7,10 @@ export const Carousel = ({album}) => {
         <img src={`/images/screenshots/${album}/2.png`} alt="2nd image" />
         <img src={`/images/screenshots/${album}/3.png`} alt="3rd image" />
         <img src={`/images/screenshots/${album}/4.png`} alt="4th image" />
+        {/* <img src={require(`/images/screenshots/${album}/1.png`)} alt="1st image" />
+        <img src={require(`/images/screenshots/${album}/2.png`)} alt="2nd image" />
+        <img src={require(`/images/screenshots/${album}/3.png`)} alt="3rd image" />
+        <img src={require(`/images/screenshots/${album}/4.png`)} alt="4th image" /> */}
     </div>
   )
 }
