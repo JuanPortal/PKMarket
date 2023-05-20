768e5571e7e76c2189efe7a0888cc59feb54b3e3
Portal <juanportalg@gmail.com>
2023-05-20 01:47:16 -0700

    carousel images fixed

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
 export const Carousel = ({album}) => {
+  let imagesURLs = []
+
+  switch (album) {
+    case 'Team':
+      imagesURLs = ['http://imgfz.com/i/D2zSC3E.png', 'http://imgfz.com/i/Y4ctzsb.png', 'http://imgfz.com/i/odZzKuh.png', 'http://imgfz.com/i/gNkT2ox.png']
+      break
+    case 'Event':
+      imagesURLs = ['http://imgfz.com/i/CtoqOiG.jpg', 'http://imgfz.com/i/I9DPU4X.png', 'http://imgfz.com/i/Qs9EutH.png', 'http://imgfz.com/i/ztoRUi5.jpg']
+      break
+    case 'Pokédex':
+      imagesURLs = ['http://imgfz.com/i/U2OGdgv.png', 'http://imgfz.com/i/F7VPGTW.png', 'http://imgfz.com/i/OFYIjob.png', 'http://imgfz.com/i/NXxzikE.png']
+      break
+    case 'Extra':
+      imagesURLs = ['http://imgfz.com/i/lHOFKpN.png', 'http://imgfz.com/i/GA7kUDw.png', 'http://imgfz.com/i/1wmxBTG.png', 'http://imgfz.com/i/vP26GVz.png']
+      break
+  }
+
   return (
     <div className='carousel'>
-        <img src={`/images/screenshots/${album}/1.png`} alt="1st image" />
-        <img src={`/images/screenshots/${album}/2.png`} alt="2nd image" />
-        <img src={`/images/screenshots/${album}/3.png`} alt="3rd image" />
-        <img src={`/images/screenshots/${album}/4.png`} alt="4th image" />
+        <img src={imagesURLs[0]} alt="1st image" />
+        <img src={imagesURLs[1]} alt="2nd image" />
+        <img src={imagesURLs[2]} alt="3rd image" />
+        <img src={imagesURLs[3]} alt="4th image" />
     </div>
   )
 }
